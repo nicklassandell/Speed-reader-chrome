f72c2453bb0ea43f8e9123f34c7103e4741925a2
Nicklas Sandell <nike.sandell@gmail.com>
2014-06-11 20:20:14 +0200

    Sexified toolbar buttons and added hide button

--- js/content.js
+++ js/content.js
@@ -1,13 +1,24 @@
 ;(function() {
 
-	var frag = create('<a id="sr-toast">Speed read</a>');
+	var html = '';
+
+	html += '<div class="sr-toolbar">';
+	html += 	'<a title="Read with Champ" id="sr-toast" class="sr-toast-button">';
+	html +=	 		'<img src="'+ chrome.extension.getURL('img/read.png') +'" alt="Read with Champ" />';
+	html +=		'</a>';
+	html += 	'<a title="Hide" id="sr-toast-hide" class="sr-toast-button">';
+	html += 		'<span>X</span>';
+	html += 	'</a>';
+	html += '</div>';
+
+	var frag = create(html);
 
 	document.body.insertBefore(frag, document.body.childNodes[0]);
 
 	var elem = document.getElementById('sr-toast');
 
 	setTimeout(function() {
-		elem.className = 'visible';
+		elem.className += ' visible';
 	}, 20);
 
 	elem.onclick = function() {
